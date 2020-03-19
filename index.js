888e1928378b96d9340837816edf437ebb7ad193
Jan Van Bruggen <JanCVanBruggen@gmail.com>
2020-03-19 10:53:22 -0600

    Increase default zoom (which fixes initial marker render)

--- index.js
+++ index.js
@@ -85,13 +85,14 @@ function render_map() {
   const map = L.map('map').setView([0, 0], 0);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    minZoom: 1,
+    minZoom: 2,
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: MAPBOX_TOKEN
   }).addTo(map);
+  map.setZoom(2);
   return map;
 }
 
